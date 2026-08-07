8a40944f8af6ab6e826d52708207a953a9e3beb5
shhyd <vertheeorumail@gmail.com>
2026-08-08 02:37:30 +0530

    revert: hero blob back to old square 1:1 frame

--- app/components/home/HomeHero.vue
+++ app/components/home/HomeHero.vue
@@ -3,10 +3,6 @@
     <div class="container home-hero__inner">
       <div class="home-hero__content">
         <UiReveal>
-          <p class="home-hero__eyebrow">
-            <UiIcon name="bolt" :size="16" />
-            Fresh, every single day
-          </p>
           <h1 class="home-hero__title display">
             From Butchers,<br />Direct To Your<br /><span>Doorstep!</span>
           </h1>
@@ -112,8 +108,8 @@ const heroImage = '/images/hero-butcher.png'
 
 .home-hero__blob {
   position: relative;
-  width: min(100%, 500px);
-  aspect-ratio: 3 / 2;
+  width: min(100%, 460px);
+  aspect-ratio: 1;
   border-radius: 44% 56% 58% 42% / 44% 42% 58% 56%;
   background: linear-gradient(145deg, var(--teal) 0%, var(--teal-deep) 100%);
   display: flex;
